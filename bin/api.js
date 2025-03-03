f337c7d5b4dbf961471d54ee12c154c3cfc7ba6f
SkunkPlatform <167719548+SkunkPlatform@users.noreply.github.com>
2025-03-03 17:27:09 -0300

    Update api.js

--- bin/api.js
+++ bin/api.js
@@ -3,9 +3,9 @@ import { generateAIResponse } from "https://sp-api.netlify.app/skunkoai.js";
 console.log("Imported to api.js");
 console.log("Importing from 'sp-api.netlify.app/skunkoai.js'...");
 
-export async function POST_SKUNKOAI(prompt, maxTokens) {
+export async function POST_SKUNKOAI(prompt, maxTokens, model = "default") {
     console.log("POST_SKUNKOAI function sent to generateAIResponse function.");
     
     // Return the result of generateAIResponse
-    return await generateAIResponse(prompt, maxTokens);
+    return await generateAIResponse(prompt, maxTokens, model);
 }
